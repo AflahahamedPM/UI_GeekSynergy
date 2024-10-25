21a2348e7bcc5de6982c70332ff3ed8415536d7c
AflahAhamed00 <aflahahamed777@gmail.com>
2024-10-25 15:32:13 +0530

    updated

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,5 +1,5 @@
 import image1 from "../assets/image-1.jpeg"
-import image2 from "../assets/image-2.png"
+// import image2 from "../assets/image-2.png"
 import image3 from "../assets/image-3.png"
 import image4 from "../assets/image-4.png"
 import image5 from "../assets/image-5.jpg"
@@ -18,7 +18,7 @@ export const cardDetails = [
     {
         id:"02",
         title:"Discover Kashmir & Ladak",
-        imageUrl: image2,
+        imageUrl: "https://s3-alpha-sig.figma.com/img/1ad6/22cd/fd1b7aca494f7e02145330213efd6fdc?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jyb3v5CSYvagZhckALd0XsOMCUAZyui59ZUio~H-~1ujUEBpUHtrmaab9ZqpyLZjqRh-qz8o9M09KtymhKGjVjkZmX18DpKJc8a5XGBzcpf-2zFfEwtNi~DdUKn8wo47-L4LR73FOa9~Ka0jYPzBbMFiK96GmULz5HNGThSBLoS4yAEha6hjiyNnkPsj~lYqo1a3dWOiQgKMMhXggDaW4eqcz2dgB3gZbiX9bQ-eAJBbD0W~5~YM5zNPuJoBrVwtU5JFx7Q-4C0C0c-7xfeK606QByU5~6j2mG-LkQVyJUO9Z5m61aDC~cy8KnPWW5vZfkl8Q-YLXWmWqRF88VF3~A__",
         price:price,
         stay:"4 Days | 3 Nights",
         pickup:"Srinagar Airport"
